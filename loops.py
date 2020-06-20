97621b6887cf3ec0a13d3747bec6ce81097d9b38
Tiffany Joa <46694937+tiffanyjoa8@users.noreply.github.com>
2020-06-19 18:38:09 -0700

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,19 @@
+print("Day 1")
+print("Day 2")
+print("Day 3")
+print("Day 4")
+print("Day 5")
+print("Day 6")
+print("Day 7")
+print("Day 8")
+print("Day 9")
+print("Day 10")
+
+#zero through nine
+for num in range (10):
+  print ("Day {}".format(num))
+
+#one through 10
+for num in range (1, 11):
+  print ("Day {}".format(num))
+
